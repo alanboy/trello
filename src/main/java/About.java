dc36f3c79614a30428db1f00fceebc49d9a9ff52
Alan Gonzalez <alanboy@alanboy.net>
2017-07-28 01:47:43 -0700

    Add msi installer to dist folder

--- src/main/java/About.java
+++ src/main/java/About.java
@@ -1,7 +1,7 @@
 /* Auto generated code, your changes will be overwritten when built by gradle */
 public class About { 
-    static final String BuiltByMachine = "DESKTOP-VT4BSKK"; 
+    static final String BuiltByMachine = "alanboy"; 
     static final String BuiltByUser = "AlanGon"; 
-    static final String DateTime = "03-22-2017 23:01:31"; 
-    static final String Version = "0.3.20170322"; 
+    static final String DateTime = "07-28-2017 01:35:20"; 
+    static final String Version = "0.3.20170728"; 
 }
