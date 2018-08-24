03ceb1cadd96d7f0f363e74721a3aebd99558e1a
Alan Gonzalez <alanboy@alanboy.net>
2018-08-24 12:26:38 -0700

    add msi again

--- src/main/java/About.java
+++ src/main/java/About.java
@@ -2,6 +2,6 @@
 public class About { 
     static final String BuiltByMachine = "alanboy2"; 
     static final String BuiltByUser = "alan"; 
-    static final String DateTime = "08-17-2018 10:24:57"; 
-    static final String Version = "0.3.20180817"; 
+    static final String DateTime = "08-23-2018 10:51:01"; 
+    static final String Version = "0.3.20180823"; 
 }
